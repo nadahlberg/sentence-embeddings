2642950c78d23b35be8702c481a3ab4b1cff7f43
Nathan Dahlberg <58701810+nadahlberg@users.noreply.github.com>
2021-12-04 08:53:09 -0500

    formatting

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
 
 setup(
-	name='sentence-embeddings',
-	version='0.0.1',    
-	description='Search and visualize sentence embeddings.',
-	url='https://github.com/nadahlberg/sentence-embeddings',
-	author='Nathan Dahlberg',
-	author_email='nadahlberg@gmail.com',
-	package_dir={'': 'src'},
-	packages=['sentence_embeddings'],
-	install_requires=[
-		'faiss-cpu',
-		'sklearn',
-		'plotly',
-	],
+    name='sentence-embeddings',
+    version='0.0.1',    
+    description='Search and visualize sentence embeddings.',
+    url='https://github.com/nadahlberg/sentence-embeddings',
+    author='Nathan Dahlberg',
+    author_email='nadahlberg@gmail.com',
+    package_dir={'': 'src'},
+    packages=['sentence_embeddings'],
+    install_requires=[
+        'faiss-cpu',
+        'sklearn',
+        'plotly',
+    ],
 )
